67b83a61969a4df52ce3a5eba92e9c6257edf1c1
Joel <joelmounts@gmail.com>
2022-05-13 18:42:21 -0700

    added category buttons and functionality-- but no pictures

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu'
 import Categories from './Categories'
 import items from './data'
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState((allCategories))
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return
+    }
+    const newItems = items.filter((item) => item.category
+      === category);
+    setMenuItems(newItems)
+  }
 
   return (
     <main>
@@ -14,6 +26,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
